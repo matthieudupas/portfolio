d3031e27dcb6c1a0c4800b104d5d4c7506204741
matthieu <matthieudupas@outlook.fr>
2025-06-29 14:07:17 +0200

    feat: add Education section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Hero from "./sections/hero/Hero.tsx";
 import About from "./sections/about/About.tsx";
+import Education from "./sections/education/Education.tsx";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
         <Hero/>
         <About/>
+        <Education/>
     </>
   )
 }
---
+++ src/sections/education/Education.tsx
@@ -0,0 +1,9 @@
+import styles from './Education.module.css'
+
+export default function Education() {
+    return (
+        <section className={styles.education}>
+            <h1>Formation</h1>
+        </section>
+    );
+}

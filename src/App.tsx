5bfcef4db6b58e142bf321455a8f49d982907401
matthieu <matthieudupas@outlook.fr>
2025-06-29 14:12:17 +0200

    feat: add Experience section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Hero from "./sections/hero/Hero.tsx";
 import About from "./sections/about/About.tsx";
 import Education from "./sections/education/Education.tsx";
+import Experience from "./sections/experience/Experience.tsx";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
         <Hero/>
         <About/>
         <Education/>
+        <Experience/>
     </>
   )
 }
---
+++ src/sections/experience/Experience.tsx
@@ -0,0 +1,9 @@
+import styles from './Experience.module.css';
+
+export default function Experience() {
+    return (
+        <section>
+            <h1>Expériences Professionnelles</h1>
+        </section>
+    );
+}
